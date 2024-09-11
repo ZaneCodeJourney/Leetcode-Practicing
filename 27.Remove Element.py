a3bbbb9c4212ed87976c393af503cfdb41e0cce8
Ziyang Wen <wenziyang007@gmail.com>
2024-09-11 21:42:17 +0800

    9.11

---
+++ 27.Remove Element.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        for j in range(0, len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        return i
+        
+# @lc code=end
+
