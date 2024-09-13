563e8674e32ebfea920dcbcf58f9a15b3dc62be8
Ziyang Wen <wenziyang007@gmail.com>
2024-09-13 22:10:22 +0800

    fast and slow pointers

--- 19.Remove Nth Node From End of List.py
+++ 19.Remove Nth Node From End of List.py
@@ -13,19 +13,15 @@ class ListNode:
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)
-        size = 0
-        cur = head
-        while cur:
-            cur = cur.next
-            size += 1
-        num = size - n
-        prev = dummy
-        cur = head
-        for _ in range(num): #num = 1
-            prev = cur
-            cur = cur.next
-        prev.next = cur.next
+        slow = fast = dummy
+        for i in range(n + 1):
+            fast = fast.next
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
         return dummy.next
+    
            
             
 # @lc code=end
