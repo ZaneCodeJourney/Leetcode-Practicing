edbdbad9afd9ea1cdf1ed0f28f248f90f87d9b06
Ziyang Wen <wenziyang007@gmail.com>
2024-09-14 17:18:00 +0800

    Better solution

--- 349.两个数组的交集.py
+++ 349.两个数组的交集.py
@@ -7,18 +7,18 @@
 # @lc code=start
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # return list(set(nums1) & set(num2))
-        a,b=dict(),dict()
-        for char in nums1:
-            if char not in a:
-                a[char] = 1
-        for char in nums2:
-            if char not in b:
-                b[char] = 1
-        result = []
-        for key in a:
-            if a.get(key, 0) and b.get(key, 0):
-                result.append(key)
-        return result
+        return list(set(nums1) & set(num2))
+        # a,b=dict(),dict()
+        # for char in nums1:
+        #     if char not in a:
+        #         a[char] = 1
+        # for char in nums2:
+        #     if char not in b:
+        #         b[char] = 1
+        # result = []
+        # for key in a:
+        #     if a.get(key, 0) and b.get(key, 0):
+        #         result.append(key)
+        # return result
 # @lc code=end
 
