19bf11572391b7970237b9dfa417332f30ef7bfe
Ziyang Wen <wenziyang007@gmail.com>
2024-09-18 17:49:37 +0800

    4Sum Double pointer

--- 18.4Sum.py
+++ 18.4Sum.py
@@ -7,20 +7,37 @@
 
 # @lc code=start
 class Solution:
+
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        freq = {}
-        for num in nums:
-            freq[num] = freq.get(num, 0)
-        ans = set()
-        for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
-                    val = target - nums[i] - nums[k] - nums[j]
-                    if val in freq:
-                        count = (nums[i] == val) + (nums[k] == val) + (nums[j] == val)
-                        if count <= freq[val]:
-                            ans.add(tuple(sorted([nums[i], nums[j], nums[k], val])))
-        return [list(x) for x in ans]
+        nums.sort()
+        n = len(nums)
+        result = []
+        for i in range(n):
+            if nums[i] > target and nums[i] > 0 and target > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] > target and target > 0:
+                    break
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left, right = j + 1, n - 1
+                while left < right:
+                    s = nums[i] + nums[j] + nums[left] + nums[right]
+                    if s == target:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+                    elif s < target:
+                        left += 1
+                    else:
+                        right -= 1
+        return result
 
 
 # @lc code=end
