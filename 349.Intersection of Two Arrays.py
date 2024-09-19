bba4c1d78b8e6b2d78f7aa3485ce6e93a430afe5
Ziyang Wen <wenziyang007@gmail.com>
2024-09-19 21:32:55 +0800

    Intersection of Two Arrays

--- 349.Intersection of Two Arrays.py
+++ 349.Intersection of Two Arrays.py
@@ -4,21 +4,20 @@
 # [349] Intersection of Two Arrays
 #
 
+
 # @lc code=start
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1) & set(num2))
-        # a,b=dict(),dict()
-        # for char in nums1:
-        #     if char not in a:
-        #         a[char] = 1
-        # for char in nums2:
-        #     if char not in b:
-        #         b[char] = 1
-        # result = []
-        # for key in a:
-        #     if a.get(key, 0) and b.get(key, 0):
-        #         result.append(key)
-        # return result
-# @lc code=end
+        res = []
+        nums1dict, nums2dict = {}, {}
+        for num in nums1:
+            nums1dict[num] = 1
+        for num in nums2:
+            nums2dict[num] = 1
+        for key in nums1dict:
+            if nums1dict.get(key, 0) == nums2dict.get(key, 0):
+                res.add(key)
+        return res
 
+
+# @lc code=end
