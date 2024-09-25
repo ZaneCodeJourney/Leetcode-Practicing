b57271ba80ddca8be04c57fce246c1dfd81b6239
Ziyang Wen <wenziyang007@gmail.com>
2024-09-25 09:56:08 +0800

    Length-based solution

--- 102.Binary Tree Level Order Traversal.py
+++ 102.Binary Tree Level Order Traversal.py
@@ -21,20 +21,19 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        levels = []
-
-        def traverse(node, level):
-            if not node:
-                return
-            if len(levels) == level:
-                levels.append([])
-
-            levels[level].append(node.val)
-            traverse(node.left, level + 1)
-            traverse(node.right, level + 1)
-
-        traverse(root, 0)
-        return levels
+        queue = deque([root])
+        result = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                cur = queue.popleft()
+                level.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            result.append(level)
+        return result
 
 
 # @lc code=end
