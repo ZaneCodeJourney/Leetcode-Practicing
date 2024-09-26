10bac47560673c0d02fbbb4046ce2fda74c52e47
Ziyang Wen <wenziyang007@gmail.com>
2024-09-26 19:36:23 +0800

    Iterative Solution

--- 101.Symmetric Tree.py
+++ 101.Symmetric Tree.py
@@ -9,32 +9,31 @@
 from collections import deque
 
 
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.compare(root.left, root.right)
 
-        queue = deque([root.left, root.right])
+    def compare(self, left, right):
+        if left == None and right != None:
+            return False
+        elif left != None and right == None:
+            return False
+        elif left == None and right == None:
+            return True
+        elif left.val != right.val:
+            return False
 
-        while queue:
-            left = queue.popleft()
-            right = queue.popleft()
-
-            if not left and not right:
-                continue
-
-            if not left or not right or left.val != right.val:
-                return False
-
-            queue.append(left.left)
-            queue.append(right.right)
-            queue.append(left.right)
-            queue.append(right.left)
-
-        return True
+        outside = self.compare(left.left, right.right)
+        inside = self.compare(left.right, right.left)
+        isSame = inside and outside
+        return isSame
 
 
 # @lc code=end
