b94a27f26b0c6142836b9d0ec46dab5114bd6bc8
Ziyang Wen <wenziyang007@gmail.com>
2024-09-26 19:51:11 +0800

    More concise code

--- 100.Same Tree.py
+++ 100.Same Tree.py
@@ -14,20 +14,13 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        return self.compare(p, q)
-
-    def compare(self, left, right):
-        if left == None and right == None:
-            return True
-        elif left is None or right is None:
-            return False
-        elif left.val != right.val:
-            return False
-
-        outside = self.compare(left.left, right.left)
-        inside = self.compare(left.right, right.right)
-        isSame = inside and outside
-        return isSame
+        if p is None or q is None:
+            return p is q
+        return (
+            p.val == q.val
+            and self.isSameTree(p.left, q.left)
+            and self.isSameTree(p.right, q.right)
+        )
 
 
 # @lc code=end
