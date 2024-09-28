97bf8baf3edb0b3d37cac1abac677c42dd0833bb
Ziyang Wen <wenziyang007@gmail.com>
2024-09-28 23:05:39 +0800

    559 Using Stack

--- 559.Maximum Depth of N-ary Tree.py
+++ 559.Maximum Depth of N-ary Tree.py
@@ -21,9 +21,13 @@ class Solution:
     def maxDepth(self, root: "Node") -> int:
         if not root:
             return 0
-        max_depth = 1
-        for child in root.children:
-            max_depth = max(max_depth, self.maxDepth(child) + 1)
+        max_depth = 0
+        stack = [(root, 1)]
+        while stack:
+            node, depth = stack.pop()
+            max_depth = max(max_depth, depth)
+            for child in node.children:
+                stack.append((child, depth + 1))
         return max_depth
 
 
