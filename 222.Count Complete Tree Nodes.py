ac26c3d10a43f21314481160fb6d5df613eae70a
Ziyang Wen <wenziyang007@gmail.com>
2024-09-29 11:51:47 +0800

    Using the properties of a complete binary tree

--- 222.Count Complete Tree Nodes.py
+++ 222.Count Complete Tree Nodes.py
@@ -16,21 +16,20 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
-    def countNodes(self, root: Optional[TreeNode]) -> int:
+class Solution:  # 利用完全二叉树特性
+    def countNodes(self, root: TreeNode) -> int:
         if not root:
             return 0
-        queue = deque([root])
         count = 0
-        while queue:
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                count += 1
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-        return count
+        left = root.left
+        right = root.right
+        while left and right:
+            count += 1
+            left = left.left
+            right = right.right
+        if not left and not right:  # 如果同时到底说明是满二叉树，反之则不是
+            return (2 << count) - 1
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
 
 
 # @lc code=end
