193b137f9d6d41d0565921f4b11755498613c3af
Ziyang Wen <wenziyang007@gmail.com>
2024-12-08 00:17:12 +0800

    Level method

---
+++ 513.Find Bottom Left Tree Value Level Traversal.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode.cn id=513 lang=python3
+#
+# [513] 找树左下角的值
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from collections import deque
+
+
+class Solution:
+    def findBottomLeftValue(self, root):
+        if root is None:
+            return 0
+        queue = deque()
+        queue.append(root)
+        result = 0
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if i == 0:
+                    result = node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return result
+
+
+# @lc code=end
